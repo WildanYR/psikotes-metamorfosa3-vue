01ebf512dbdb5a24ec77b84fc4b6c302a4cee4d6
WildanYR <wildanyahya99@gmail.com>
2022-02-09 08:58:51 +0700

    add global notification

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  XIcon,
+  EmojiHappyIcon,
+  EmojiSadIcon,
+  ExclamationCircleIcon,
+  InformationCircleIcon
+} from '@heroicons/vue/outline'
 </script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <router-view></router-view>
+  <notifications position="top center">
+    <template #body="props">
+      <div
+        :class="[
+          'mt-4 flex items-start justify-between rounded-lg border p-4',
+          props.item.type === 'success'
+            ? 'border-green-200 bg-green-50'
+            : props.item.type === 'error'
+            ? 'border-red-200 bg-red-50'
+            : props.item.type === 'warning'
+            ? 'border-amber-200 bg-amber-50'
+            : 'border-blue-200 bg-blue-50'
+        ]"
+      >
+        <div class="flex items-center justify-center gap-3">
+          <EmojiHappyIcon
+            v-if="props.item.type === 'success'"
+            class="h-7 w-7 text-green-700"
+          />
+          <EmojiSadIcon
+            v-else-if="props.item.type === 'error'"
+            class="h-7 w-7 text-red-700"
+          />
+          <ExclamationCircleIcon
+            v-else-if="props.item.type === 'warning'"
+            class="h-7 w-7 text-amber-700"
+          />
+          <InformationCircleIcon v-else class="h-7 w-7 text-blue-700" />
+          <div>
+            <p
+              :class="[
+                'font-bold',
+                props.item.type === 'success'
+                  ? 'text-green-900'
+                  : props.item.type === 'error'
+                  ? 'text-red-900'
+                  : props.item.type === 'warning'
+                  ? 'text-amber-900'
+                  : 'text-blue-900'
+              ]"
+            >
+              {{ props.item.title }}
+            </p>
+            <p class="text-sm">{{ props.item.text }}</p>
+          </div>
+        </div>
+        <button class="text-gray-600 hover:text-gray-800" @click="props.close">
+          <XIcon class="h-4 w-4" />
+        </button>
+      </div>
+    </template>
+  </notifications>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import Notifications from '@kyvg/vue3-notification'
 import './index.css'
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).use(Notifications).mount('#app')
